c1a737c921c7fd33e20753de5047290192e5b2dd
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-03-01 15:11:48 -0800

    refactored login.js - functional component

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,108 +1,103 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Input from '../../components/Form/Input/Input';
 import Button from '../../components/Button/Button';
 import { required, length, email } from '../../util/validators';
 import Auth from './Auth';
 
-class Login extends Component {
-  state = {
-    loginForm: {
-      email: {
-        value: '',
-        valid: false,
-        touched: false,
-        validators: [required, email]
-      },
-      password: {
-        value: '',
-        valid: false,
-        touched: false,
-        validators: [required, length({ min: 5 })]
-      },
-      formIsValid: false
-    }
-  };
+const Login = ({ loading, onLogin }) => {
 
-  inputChangeHandler = (input, value) => {
-    this.setState(prevState => {
-      let isValid = true;
-      for (const validator of prevState.loginForm[input].validators) {
-        isValid = isValid && validator(value);
-      }
-      const updatedForm = {
-        ...prevState.loginForm,
-        [input]: {
-          ...prevState.loginForm[input],
-          valid: isValid,
-          value: value
-        }
-      };
-      let formIsValid = true;
-      for (const inputName in updatedForm) {
-        formIsValid = formIsValid && updatedForm[inputName].valid;
-      }
-      return {
-        loginForm: updatedForm,
-        formIsValid: formIsValid
-      };
+    const [loginForm, setLoginForm] = useState({
+        email: {
+            value: '',
+            valid: false,
+            touched: false,
+            validators: [required, email]
+        },
+        password: {
+            value: '',
+            valid: false,
+            touched: false,
+            validators: [required, length({ min: 5 })]
+        },
+        formIsValid: false
     });
-  };
 
-  inputBlurHandler = input => {
-    this.setState(prevState => {
-      return {
-        loginForm: {
-          ...prevState.loginForm,
-          [input]: {
-            ...prevState.loginForm[input],
-            touched: true
-          }
-        }
-      };
-    });
-  };
+    const inputChangeHandler = (input, value) => {
+        setLoginForm(prevState => {
+            let isValid = true;
+            for (const validator of prevState[input].validators) {
+                isValid = isValid && validator(value);
+            }
+            const updatedForm = {
+                ...prevState,
+                [input]: {
+                    ...prevState[input],
+                    valid: isValid,
+                    value: value
+                }
+            };
+            let formIsValid = true;
+            for (const inputName in updatedForm) {
+                formIsValid = formIsValid && updatedForm[inputName].valid;
+            }
+            return {
+                ...updatedForm,
+                formIsValid: formIsValid
+            };
+        });
+    };
 
-  render() {
+    const inputBlurHandler = input => {
+        setLoginForm(prevState => {
+            return {
+                ...prevState,
+                [input]: {
+                    ...prevState[input],
+                    touched: true
+                }
+            };
+        });
+    };
+
     return (
-      <Auth>
-        <form
-          onSubmit={e =>
-            this.props.onLogin(e, {
-              email: this.state.loginForm.email.value,
-              password: this.state.loginForm.password.value
-            })
-          }
-        >
-          <Input
-            id="email"
-            label="Your E-Mail"
-            type="email"
-            control="input"
-            onChange={this.inputChangeHandler}
-            onBlur={this.inputBlurHandler.bind(this, 'email')}
-            value={this.state.loginForm['email'].value}
-            valid={this.state.loginForm['email'].valid}
-            touched={this.state.loginForm['email'].touched}
-          />
-          <Input
-            id="password"
-            label="Password"
-            type="password"
-            control="input"
-            onChange={this.inputChangeHandler}
-            onBlur={this.inputBlurHandler.bind(this, 'password')}
-            value={this.state.loginForm['password'].value}
-            valid={this.state.loginForm['password'].valid}
-            touched={this.state.loginForm['password'].touched}
-          />
-          <Button design="raised" type="submit" loading={this.props.loading}>
-            Login
-          </Button>
-        </form>
-      </Auth>
+        <Auth>
+            <form
+                onSubmit={e =>
+                    onLogin(e, {
+                        email: loginForm.email.value,
+                        password: loginForm.password.value
+                    })
+                }
+            >
+                <Input
+                    id="email"
+                    label="Your E-Mail"
+                    type="email"
+                    control="input"
+                    onChange={inputChangeHandler}
+                    onBlur={inputBlurHandler.bind('email')}
+                    value={loginForm.email.value}
+                    valid={loginForm.email.valid}
+                    touched={loginForm.email.touched}
+                />
+                <Input
+                    id="password"
+                    label="Password"
+                    type="password"
+                    control="input"
+                    onChange={inputChangeHandler}
+                    onBlur={inputBlurHandler.bind('password')}
+                    value={loginForm.password.value}
+                    valid={loginForm.password.valid}
+                    touched={loginForm.password.touched}
+                />
+                <Button design="raised" type="submit" loading={loading}>
+                    Login
+                </Button>
+            </form>
+        </Auth>
     );
-  }
 }
 
 export default Login;
