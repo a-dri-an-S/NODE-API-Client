fd8e714442349a99d94efc0b3d3a65e626bfc3a1
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-03-04 23:04:08 -0800

    added proptypes to auth

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Auth.css';
 
 const auth = ({ children }) => <section className="auth-form">{children}</section>;
+
+auth.propTypes = {
+    children: PropTypes.node.isRequired
+}
 
 export default auth;
