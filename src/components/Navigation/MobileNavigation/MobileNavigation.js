797143ebd2d55156215875b666b52e569415e9b2
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-03-06 16:13:10 -0800

    destructured props + added proptype checking to mobileNavigation

--- src/components/Navigation/MobileNavigation/MobileNavigation.js
+++ src/components/Navigation/MobileNavigation/MobileNavigation.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import NavigationItems from '../NavigationItems/NavigationItems';
 import './MobileNavigation.css';
 
-const mobileNavigation = props => (
-  <nav className={['mobile-nav', props.open ? 'open' : ''].join(' ')}>
+const mobileNavigation = ({ open, mobile, onChooseItem, onLogout, isAuth }) => (
+  <nav className={['mobile-nav', open ? 'open' : ''].join(' ')}>
     <ul
-      className={['mobile-nav__items', props.mobile ? 'mobile' : ''].join(' ')}
+      className={['mobile-nav__items', mobile ? 'mobile' : ''].join(' ')}
     >
       <NavigationItems
         mobile
-        onChoose={props.onChooseItem}
-        isAuth={props.isAuth}
-        onLogout={props.onLogout}
+        onChoose={onChooseItem}
+        isAuth={isAuth}
+        onLogout={onLogout}
       />
     </ul>
   </nav>
 );
+
+mobileNavigation.propTypes = {
+  open: PropTypes.bool.isRequired,
+  mobile: PropTypes.node.isRequired,
+  onChooseItem: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired,
+  isAuth: PropTypes.bool.isRequired
+};
 
 export default mobileNavigation;
