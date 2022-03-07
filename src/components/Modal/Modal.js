19f887ad863f4ee74b6666afd6faa693d796ef30
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-03-06 16:38:09 -0800

    destructured props + added proptype checking

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 import Button from '../Button/Button';
 import './Modal.css';
 
-const modal = props =>
+const modal = ({ title, onCancelModal, onAcceptModal, acceptEnabled, isLoading, children }) =>
   ReactDOM.createPortal(
     <div className="modal">
       <header className="modal__header">
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
       </header>
-      <div className="modal__content">{props.children}</div>
+      <div className="modal__content">{children}</div>
       <div className="modal__actions">
-        <Button design="danger" mode="flat" onClick={props.onCancelModal}>
+        <Button design="danger" mode="flat" onClick={onCancelModal}>
           Cancel
         </Button>
         <Button
           mode="raised"
-          onClick={props.onAcceptModal}
-          disabled={!props.acceptEnabled}
-          loading={props.isLoading}
+          onClick={onAcceptModal}
+          disabled={!acceptEnabled}
+          loading={isLoading}
         >
           Accept
         </Button>
@@ -27,5 +28,14 @@ const modal = props =>
     </div>,
     document.getElementById('modal-root')
   );
+
+  modal.propTypes = {
+    title: PropTypes.string.isRequired,
+    onCancelModal: PropTypes.func.isRequired,
+    onAcceptModal: PropTypes.func.isRequired,
+    acceptEnabled: PropTypes.bool.isRequired,
+    isLoading: PropTypes.bool.isRequired,
+    children: PropTypes.node.isRequired
+  };
 
 export default modal;
